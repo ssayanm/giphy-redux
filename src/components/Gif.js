043b3cacf20e2e5e44c868c29a166c3e6d4ce5e5
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-12-30 18:04:36 +0530

    gif.js

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,20 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Gif = ({ image }) => {
-	//const { image } = props;
-	return (
-	    <div className="bg-light-blue tc dib br1 pa1 ma2 grow bw1 shadow-1">
-	    	
-	    	 <img alt='gifs' src={image} />
-	    	  <div>
-        
-      </div>
-	    </div>
+  //const { image } = props;
+  return (
+    <div className="bg-light-blue tc dib br1 pa1 ma2 grow bw1 shadow-1">
+      <img alt="gifs" src={image} />
+    </div>
   );
-}
-
-
+};
 
 export default Gif;
-
-
